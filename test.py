de1dec484643a60c19ca5af7592aa28b83ba552c
Dolbyan <pepik767@wp.pl>
2024-08-18 00:51:18 +0200

    work?

--- test.py
+++ test.py
@@ -8,17 +8,17 @@ class TestCrud(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-        REQUEST_COUNT._value.set(0)
-        ERROR_RATE._value.set(0)
-        PASS_RATE._value.set(0)
-        REQUEST_LATENCY._sum.set(0)
-        REQUEST_LATENCY._count.set(0)
+        with app.app_context():
+            REQUEST_COUNT._value.set(0)
+            ERROR_RATE._value.set(0)
+            PASS_RATE._value.set(0)
+            REQUEST_LATENCY._sum.set(0)
+            REQUEST_LATENCY._count.set(0)
 
     def test_latency_endpoint(self):
         response = self.app.get('/latency')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Latency endpoint', response.data)
-
         self.assertEqual(REQUEST_COUNT._value.get(), 1)
         self.assertEqual(REQUEST_LATENCY._count.get(), 1)
 
@@ -26,7 +26,6 @@ class TestCrud(unittest.TestCase):
         response = self.app.get("/get")
         self.assertEqual(response.status_code, 500)
         self.assertIn(b"Error endpoint", response.data)
-
         self.assertEqual(REQUEST_COUNT._value.get(), 1)
         self.assertEqual(ERROR_RATE._value.get(), 1)
 
@@ -34,7 +33,6 @@ class TestCrud(unittest.TestCase):
         response = self.app.get('/timeout')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'timeout endpoint', response.data)
-
         self.assertEqual(REQUEST_COUNT._value.get(), 1)
         self.assertEqual(PASS_RATE._value.get(), 1)
         self.assertEqual(REQUEST_LATENCY._count.get(), 1)
@@ -42,7 +40,6 @@ class TestCrud(unittest.TestCase):
         response = self.app.get('/timeout5')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'timeout5 endpoint', response.data)
-
         self.assertEqual(REQUEST_COUNT._value.get(), 1)
         self.assertEqual(PASS_RATE._value.get(), 1)
         self.assertEqual(REQUEST_LATENCY._count.get(), 1)
@@ -50,7 +47,6 @@ class TestCrud(unittest.TestCase):
         response = self.app.get('/mstimeout')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Latency endpoint', response.data)
-
         self.assertEqual(REQUEST_COUNT._value.get(), 1)
         self.assertEqual(PASS_RATE._value.get(), 1)
         self.assertEqual(REQUEST_LATENCY._count.get(), 1)
@@ -58,7 +54,6 @@ class TestCrud(unittest.TestCase):
     def test_metrics_endpoint(self):
         response = self.app.get('/metrics')
         self.assertEqual(response.status_code, 200)
-
         self.assertIn(b'request_latency_seconds_count', response.data)
         self.assertIn(b'request_count_total', response.data)
         self.assertIn(b'error_rate_total', response.data)
